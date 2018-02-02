77b194eedeae8ec1a8b7de80e91469d2513cc76f
Victor Moura <victor_cmoura@hotmail.com>
2018-02-02 15:37:13 -0200

    Creating GETs to fetch car info over time

--- app/controllers/car_informations_controller.rb
+++ app/controllers/car_informations_controller.rb
@@ -1,5 +1,6 @@
 class CarInformationsController < ApplicationController
   before_action :set_car_information, only: [:show, :update, :destroy]
+  before_action :set_car_info_data, only: [:get_average_rpm, :get_time_traveled, :get_odometer, :get_distance_traveled, :get_average_consumption, :get_all_data]
 
   # GET /car_informations
   def index
@@ -29,6 +30,79 @@ class CarInformationsController < ApplicationController
     else
       render json: @car_information.errors, status: :unprocessable_entity
     end
+  end
+
+  def get_average_consumption
+    @average_consumption_data = []
+
+    @car_info_data.each do |data|
+      @average_consumption_data.append({
+        :data => data.average_fuel_consumption,
+        :start_time => data.start_time
+        })
+    end
+
+    render json: @average_consumption_data
+  end
+
+  def get_average_rpm
+    @average_rpm_data = []
+
+    @car_info_data.each do |data|
+      @average_rpm_data.append({
+        :data => data.average_rpm,
+        :start_time => data.start_time
+        })
+    end
+
+    render json: @average_rpm_data
+  end
+
+  def get_distance_traveled
+    @distance_traveled_data = []
+
+    @car_info_data.each do |data|
+      @distance_traveled_data.append({
+        :data => data.distance_traveled,
+        :start_time => data.start_time
+        })
+    end
+
+    render json: @distance_traveled_data
+  end
+
+  def get_time_traveled
+    @get_time_traveled_data = []
+
+    @car_info_data.each do |data|
+      @get_time_traveled_data.append({
+        :data => (data.end_time - data.start_time)/1.minute,
+        :start_time => data.start_time
+        })
+    end
+
+    render json: @get_time_traveled_data
+  end
+
+  def get_all_data
+    @all_data = []
+
+    @car_info_data.each do |data|
+      @all_data.append({
+        :average_fuel_consumption => data.average_fuel_consumption,
+        :average_rpm => data.average_rpm,
+        :distance_traveled => data.distance_traveled,
+        :time_traveled => (data.end_time - data.start_time)/1.minute,
+        :start_time => data.start_time
+        })
+    end
+
+    render json: @all_data
+  end
+
+  def set_car_info_data
+    @car = Car.find_by_vin(params[:vin])
+    @car_info_data = CarInformation.where :car_id => @car.id
   end
 
   # PATCH/PUT /car_informations/1
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   resources :car_informations
   resources :cars
+
+  get '/get_average_consumption_data/:vin', to: 'car_informations#get_average_consumption'
+  get '/get_average_rpm_data/:vin', to: 'car_informations#get_average_rpm'
+  get '/get_distance_traveled_data/:vin', to: 'car_informations#get_distance_traveled'
+  get '/get_time_traveled_data/:vin', to: 'car_informations#get_time_traveled'
+  get '/get_all_data/:vin', to: 'car_informations#get_all_data'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
